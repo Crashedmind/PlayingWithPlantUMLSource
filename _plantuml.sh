8a1b9bdf56dfaaea4aebf13d42157e3098e5fd87
Crashedmind <crashedmind@gmail.com>
2020-05-19 22:54:02 +0100

    add openiconic

--- _plantuml.sh
+++ _plantuml.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-java -jar /usr/share/plantuml/plantuml.jar -config /home/documentation/docs/plantuml/plantuml_cfg.puml #location as per Dockerfile
+java -jar /usr/share/plantuml/plantuml.jar #for guide use no config - it breaks things like listopeniconic
+#java -jar /usr/share/plantuml/plantuml.jar -config /home/documentation/docs/plantuml/plantuml_cfg.puml #location as per Dockerfile
 #java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar -tsvg -failfast2 "$@"
 
 
